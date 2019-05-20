931eca692e045da604618c0c73c8a3224c54c10e
Murphy Huang <chao.huang@danielwellington.com>
2019-05-20 11:32:28 +0800

    homework1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,91 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import dayjs from 'dayjs'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class CommentsInput extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      value: ''
+    }
+  }
+
+  handleChange(e) {
+    this.setState({
+      value: e.target.value
+    })
+  }
+
+  handleClick(e) {
+    const message = this.state.value
+    if (!message) { return }
+    this.props.add({
+      time: dayjs().format('YYYY-MM-DD HH:mm:ss'),
+      message
+    })
+    this.setState({ value: '' })
+  }
+
+  render() {
+    return (
+      <div>
+        <input value={this.state.value} type={"text"} onChange={this.handleChange.bind(this)}/>
+        <button onClick={this.handleClick.bind(this)}>Submit</button>
+      </div>
+    )
+  }
+}
+
+class CommentsDisplay extends React.Component {
+  render() {
+    return (
+      <div className={'CommentsDisplay'}>
+        <h4>Comments</h4>
+        <hr/>
+        <ul>
+          { this.props.children.map((item, index) => (
+            <li key={`comment-${index}`}>
+              <span>{ item.time }</span>
+              <br/>
+              {item.message}
+              <hr/>
+            </li>)
+          ) }
+        </ul>
+      </div>
+    )
+  }
+}
+
+class App extends React.Component{
+  constructor(props) {
+    super(props)
+    this.state = {
+      commentList: []
+    }
+  }
+
+  addComment(content) {
+    this.setState((prevState) => {
+      prevState.commentList.unshift(content)
+      return prevState
+    })
+  }
+
+  render() {
+    return (
+      <div className={"App"}>
+        <header className="App-header">
+          Comments Board
+        </header>
+        <div className={"App-body"}>
+          <CommentsInput add={this.addComment.bind(this)}/>
+          <CommentsDisplay>
+            {this.state.commentList}
+          </CommentsDisplay>
+        </div>
+      </div>)
+  }
 }
 
 export default App;
